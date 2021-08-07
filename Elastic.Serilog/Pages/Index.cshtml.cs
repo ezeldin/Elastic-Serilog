64c9c178917757364721b26695ff672698c4aec2
ezeldin <ezeldin_hussein@hotmail.com>
2021-08-07 22:36:13 +0300

    init

---
+++ Elastic.Serilog/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elastic.Serilog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("HomeController Index executed at {date}", DateTime.UtcNow);
+            _logger.LogInformation("Elastic.Serilog App Started at {date}", DateTime.UtcNow);
+        }
+    }
+}
